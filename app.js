2051ab3ca6cb429fba1a79334599fd6e9792b193
Michael Ip <ip.w.michael@gmail.com>
2014-10-09 16:08:04 -0400

    almost functional

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 
 function newTS() { return new Date().getTime(); };
 
-
-// var myNum = Date.now;
-//var millis = newTS()-1412821200000;
-
 var total = 0;
 var clicks = 0;
 
@@ -16,10 +12,93 @@ var allClicks = [];
 
 var theTime = 0;
 //need to initialize once so that i can keep a time running from zero or something
-//var millis = newTS() % 10000;
 var i = 0;
-//var first = true;
+var origin = 0;
+var gameOver = false;
 
+
+function myFunc(){
+	if(clicks==0){//first button click
+
+		//store origin time ONLY
+		origin = newTS()%1000000;
+		allTimes[clicks]=origin;
+		//theTime = origin - newTS()%1000000;
+		update("disp", "Go!");
+
+		clicks++;
+	}else if(clicks == 1){
+		var realTime =newTS()%1000000;
+		//next is the interval between clicks
+		var next = realTime - origin;
+		update("disp", "clicks: "+ clicks + "! " +next);
+
+		allClicks[clicks-1]=next;
+		allTimes[clicks]=realTime;
+
+		total+=realTime;
+		clicks++;
+	}else if(clicks < 11){
+		//11 > clicks >= 2
+		realTime = newTS()%1000000;
+		next = realTime - allTimes[clicks-1];
+		update("disp", "clicks: "+ clicks + "! " +next);
+
+		allClicks[clicks-1] = next;
+		allTimes[clicks]=realTime;
+
+		total+=realTime;
+		clicks++;
+	}else{
+		update("disp", "ya done son");
+		gameOver = true;
+	}
+
+	if(gameOver){
+
+		//reminder: allTimes is the actual time stamps, starting with origin at 0 to clicks
+		//allClicks is intervals, starting from 0 to clicks - 1
+		for(i=0; i<clicks-1; i++){
+			total+=allClicks[i];
+		}
+		var mean = total / (clicks - 1);
+
+		for(i=)
+	}
+
+	clicks = 0;
+
+	// allTimes[clicks] = theTime;
+	// clicks++;
+
+	//in the process of fixing
+	// theTime = newTS() % 1000000;
+	// allTimes.push(theTime);
+	// total+=theTime;
+
+	// var tempTotal = 0;
+
+	// var mean = total/clicks;
+
+	// for(i=0; i < allTimes.length; i++){
+
+	// 	tempTotal+=Math.pow(allTimes[i]-mean,2);
+	// }
+
+	// var stdDev = Math.sqrt(tempTotal/allTimes.length);
+
+	// document.getElementById("num").innerHTML = "clicks: "+clicks+"\ntime: " + theTime +"\nstdDev: "+stdDev;
+}
+
+function update(id, num){
+
+	document.getElementById(id).innerHTML = num;
+}
+
+// function personController($scope) {
+//     $scope.firstName = "John";
+//     $scope.lastName = "Doe";
+// }
 
 // function personController($scope) {
 //     $scope.person = {
@@ -30,62 +109,4 @@ var i = 0;
 //     $scope.theButt = myFunc() {
 //         $scope.myVar = !$scope.myVar;
 //     };
-// }
-
-function myFunc(){
-	if(clicks==0){//first button click
-
-		//store origin time ONLY
-		var origin = newTS()%1000000;
-		allTimes[clicks]=origin;
-		//theTime = origin - newTS()%1000000;
-
-		clicks++;
-	}else if(clicks == 1){
-		var realTime =newTS()%1000000;
-		//next is the interval between clicks
-		var next = realTime - origin;
-
-		allClicks[clicks-1]=next;
-		allTimes[clicks]=realTime;
-		clicks++;
-	}else if(clicks < 11){
-		//11 > clicks >= 2
-		realTime = newTS()%1000000;
-		next = realTime - allTimes[clicks-1];
-
-		allTimes[clicks]=realTime;
-		clicks++;
-	}
-
-	allTimes[clicks] = theTime;
-	clicks++;
-
-	//in the process of fixing
-	theTime = newTS() % 1000000;
-	allTimes.push(theTime);
-	total+=theTime;
-
-	var tempTotal = 0;
-
-	var mean = total/clicks;
-
-	for(i=0; i < allTimes.length; i++){
-
-		tempTotal+=Math.pow(allTimes[i]-mean,2);
-	}
-
-	var stdDev = Math.sqrt(tempTotal/allTimes.length);
-
-	document.getElementById("num").innerHTML = "clicks: "+clicks+"\ntime: " + theTime +"\nstdDev: "+stdDev;
-}
-
-function updateDisplay(){
-
-	
-}
-
-// function personController($scope) {
-//     $scope.firstName = "John";
-//     $scope.lastName = "Doe";
 // }
