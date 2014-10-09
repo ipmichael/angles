20af9b2aa8882cb912e929e95854e28a42618408
Michael Ip <ip.w.michael@gmail.com>
2014-10-08 22:19:39 -0400

    added time stamp thing

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-if (!Date.now) {
-    Date.now = function() { return new Date().getTime(); };
-}
 
-var myNum = Date.now;
+function newTS() { return new Date().getTime(); };
+
+
+// var myNum = Date.now;
+var millis = newTS();
+
+var myDate = new Date(milliseconds)
 
 function myFunc(){
-	document.getElementById("num").innerHTML = myNum;
+	document.getElementById("num").innerHTML = millis;
 }
 
