1b1274ffe0ba3068fe7c04316ed9ed3c0c1c9339
Michael Ip <ip.w.michael@gmail.com>
2014-10-09 15:04:37 -0400

    wrote more clicky stuff

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ function newTS() { return new Date().getTime(); };
 
 var total = 0;
 var clicks = 0;
+
+//actual time stamps in milliseconds
 var allTimes = [];
+
+//time since last click
+var allClicks = [];
+
+var theTime = 0;
 //need to initialize once so that i can keep a time running from zero or something
-var origin = newTS()%100000000;
-var firstTime = origin - newTS()%1000000;
 //var millis = newTS() % 10000;
 var i = 0;
+//var first = true;
 
 
 // function personController($scope) {
@@ -27,10 +33,32 @@ var i = 0;
 // }
 
 function myFunc(){
+	if(clicks==0){//first button click
+
+		//store origin time ONLY
+		var origin = newTS()%1000000;
+		allTimes[clicks]=origin;
+		//theTime = origin - newTS()%1000000;
+
+		clicks++;
+	}else if(clicks == 1){
+		var realTime =newTS()%1000000;
+		var next = realTime - origin;
+		allClicks[clicks-1]=next;
+		allTimes[clicks]=realTime;
+		clicks++;
+	}else if(clicks < 11){
+		//11 > clicks >= 2
+		theTime = (newTS()%1000000) - allTimes[clicks-2];
+		allTimes[clicks-1]=theTime;
+		clicks++;
+	}
+
+	allTimes[clicks] = theTime;
 	clicks++;
 
 	//in the process of fixing
-	var theTime = newTS() % 1000000;
+	theTime = newTS() % 1000000;
 	allTimes.push(theTime);
 	total+=theTime;
 
