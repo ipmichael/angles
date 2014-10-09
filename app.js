35ba1270f5a1d0e4ee4bd6374038e29aca4d7f06
Michael Ip <ip.w.michael@gmail.com>
2014-10-08 23:12:14 -0400

    standard deviation
    not working atm, have to fix

--- app.js
+++ app.js
@@ -3,11 +3,50 @@ function newTS() { return new Date().getTime(); };
 
 
 // var myNum = Date.now;
-var millis = newTS();
+//var millis = newTS()-1412821200000;
 
-var myDate = new Date(milliseconds)
+var total = 0;
+var clicks = 0;
+var allTimes = [];
+var origin = newTS()%100000000;
+var firstTime = origin - newTS()%1000000;
+//var millis = newTS() % 10000;
+var i = 0;
+
+
+// function personController($scope) {
+//     $scope.person = {
+//         firstName: "John",
+//         lastName: "Doe"
+//     };
+//     $scope.myVar = true;
+//     $scope.theButt = myFunc() {
+//         $scope.myVar = !$scope.myVar;
+//     };
+// }
 
 function myFunc(){
-	document.getElementById("num").innerHTML = millis;
+	clicks++;
+
+	var theTime = newTS() % 1000000;
+	allTimes.push(theTime);
+	total+=theTime;
+
+	var tempTotal = 0;
+
+	var mean = total/clicks;
+
+	for(i=0; i < allTimes.length; i++){
+
+		tempTotal+=Math.pow(allTimes[i]-mean,2);
+	}
+
+	var stdDev = Math.sqrt(tempTotal/allTimes.length);
+
+	document.getElementById("num").innerHTML = "clicks: "+clicks+"\ntime: " + theTime +"\nstdDev: "+stdDev;
 }
 
+// function personController($scope) {
+//     $scope.firstName = "John";
+//     $scope.lastName = "Doe";
+// }
